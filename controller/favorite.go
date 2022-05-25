9f68a5bb98e0f417aefbfaad69181ef122a13224
illikea <illikea@163.com>
2022-05-25 22:17:33 +0800

    "testFavorite"

--- controller/favorite.go
+++ controller/favorite.go
@@ -12,8 +12,10 @@ func FavoriteAction(c *gin.Context) {
 
 	if _, exist := usersLoginInfo[token]; exist {
 		if action_type == "1" {
+			DemoVideos[0].IsFavorite = true
 			c.JSON(http.StatusOK, Response{StatusCode: 0, StatusMsg: "Like success"})
 		} else if action_type == "2" {
+			DemoVideos[0].IsFavorite = false
 			c.JSON(http.StatusOK, Response{StatusCode: 0, StatusMsg: "Unlike success"})
 		}
 	} else {
