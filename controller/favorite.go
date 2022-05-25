8aba87e0246333e8a86f6c4ce572966a95eb7529
illikea <illikea@163.com>
2022-05-25 16:25:33 +0800

    "testFavorite"

--- controller/favorite.go
+++ controller/favorite.go
@@ -10,7 +10,7 @@ func FavoriteAction(c *gin.Context) {
 	token := c.Query("token")
 
 	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
+		c.JSON(http.StatusOK, Response{StatusCode: 0, StatusMsg: "Action success"})
 	} else {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	}
