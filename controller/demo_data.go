4b0cd137148c9645238c9420a707fb30b5f1c018
illikea <illikea@163.com>
2022-06-01 20:48:30 +0800

    "test feed and publish"

--- controller/demo_data.go
+++ controller/demo_data.go
@@ -4,7 +4,7 @@ var DemoVideos = []Video{
 	{
 		Id:            1,
 		Author:        DemoUser,
-		PlayUrl:       "127.0.0.1:8080/static/vergil.mp4",
+		PlayUrl:       "http://127.0.0.1:8080/static/vergil.mp4",
 		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
 		FavoriteCount: 0,
 		CommentCount:  0,
