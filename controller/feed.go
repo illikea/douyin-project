5aaa1a22f3b5db8a562754fd663572b4d924055b
illikea <illikea@163.com>
2022-06-02 22:41:51 +0800

    "test publish"

--- controller/feed.go
+++ controller/feed.go
@@ -1,13 +1,15 @@
 package controller
 
 import (
-	"bytes"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
+
+const BaseUploadPath = "/public/"
 
 type FeedResponse struct {
 	Response
@@ -17,7 +19,7 @@ type FeedResponse struct {
 
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
-	//以⼆进制格式上传⽂件
+	/*//以⼆进制格式上传⽂件
 	//这是⼀个Post 参数会被返回的地址
 	uri := "http://127.0.0.1:8888/static/bear.mp4"
 	byte, err := ioutil.ReadFile("bear.mp4")
@@ -37,11 +39,56 @@ func Feed(c *gin.Context) {
 	if err != nil {
 		fmt.Println(" post err=", err)
 	}
-	fmt.Println(string(body))
+	fmt.Println(string(body))*/
+
+	http.HandleFunc("/download", handleDownload)
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("Server run fail")
+	}
 
 	c.JSON(http.StatusOK, FeedResponse{
 		Response:  Response{StatusCode: 0, StatusMsg: ""},
 		VideoList: DemoVideos,
 		NextTime:  time.Now().Unix(),
 	})
+}
+
+//文件下载
+func handleDownload(w http.ResponseWriter, request *http.Request) {
+	//文件上传只允许GET方法
+	if request.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = w.Write([]byte("Method not allowed"))
+		return
+	}
+	//文件名
+	filename := request.FormValue("filename")
+	if filename == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, "Bad request")
+		return
+	}
+	log.Println("filename: " + filename)
+	//打开文件
+	file, err := os.Open(BaseUploadPath + filename)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, "Bad request")
+		return
+	}
+	//结束后关闭文件
+	defer file.Close()
+
+	//设置响应的header头
+	w.Header().Add("Content-type", "application/octet-stream")
+	w.Header().Add("content-disposition", "attachment; filename=\""+filename+"\"")
+	//将文件写至responseBody
+	_, err = io.Copy(w, file)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, "Bad request")
+		return
+	}
 }
